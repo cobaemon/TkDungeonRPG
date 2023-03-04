d054e5640be85362ab965110007520b59a26ee01
cobaemon <59654193+cobaemon@users.noreply.github.com>
2023-03-05 02:28:29 +0900

    Update CustomCanvas.py
    コメントを追加

--- CustomCanvas.py
+++ CustomCanvas.py
@@ -3,16 +3,18 @@ import tkinter as tk
 
 class CustomCanvas(tk.Canvas):
     def __init__(self, master=None, **kwargs):
-        kwargs['highlightthickness'] = 0
+        kwargs['highlightthickness'] = 0   # キャンバスの境界線の太さを0にする
         super().__init__(master, **kwargs)
-        self._images = []
+        self._images = []   # キャンバスに追加された画像のリスト
     
-    # キャンバスに画像を追加
+    # キャンバスに画像を追加する関数
     def add_image(self, x, y, image):
+        # 画像をリストに追加し、キャンバスにも表示する
         self._images.append([image, x, y])
         self.create_image(x, y, image=image.resize(int(x*2), int(y*2)))
     
-    # キャンバスをクリアする
+    # キャンバスをクリアする関数
     def clear(self):
+        # 画像のリストとキャンバス上の画像を全て削除する
         self._images.clear()
         self.delete('all')
